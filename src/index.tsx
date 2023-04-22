adc137f1a734d9a8d0eb7f978f482a6db7afcb94
Tomasz Durałek <tomek@blogweb.pl>
2023-04-22 21:11:08 +0200

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container!);
+
+function App() {
+	return <div>Hello World!</div>;
+}
+
+root.render(<App />);
