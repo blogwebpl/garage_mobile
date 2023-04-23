86a945e8a3dba96b082f66056e92d7707eec7bf2
Tomasz Durałek <tomek@blogweb.pl>
2023-04-23 17:18:16 +0200

    Added cordova

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,34 @@
 import { createRoot } from 'react-dom/client';
+import { App } from './App';
+import { GlobalStyle } from './GlobalStyle';
+
+declare let window: any;
+declare let device: any;
 
 const container = document.getElementById('app');
 const root = createRoot(container!);
 
-function App() {
-	return <div>Hello World!</div>;
-}
-
 root.render(<App />);
+
+const renderReactDom = () => {
+	root.render(
+		<>
+			<GlobalStyle />
+			<App />
+		</>
+	);
+};
+
+if (window.cordova) {
+	document.addEventListener(
+		'deviceready',
+		() => {
+			window.screen.orientation.lock('portrait');
+			localStorage.setItem('uuid', device.uuid);
+			renderReactDom();
+		},
+		false
+	);
+} else {
+	renderReactDom();
+}
