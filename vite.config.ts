e6fe67db4a5d2cea69ea2f89b72958fdeb92f5ec
Tomasz Durałek <tomek@blogweb.pl>
2023-04-25 17:46:53 +0200

    Added node polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import nodePolyfills from 'vite-plugin-node-stdlib-browser';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  root: 'src',
-  build: {
-    outDir: '../www'
-  },
-  plugins: [react()],
+	root: 'src',
+	build: {
+		outDir: '../www'
+	},
+	plugins: [react(), nodePolyfills()],
 })
